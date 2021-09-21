13e8b45cabec4cd331c44d0bce1e3687a2ad0d40
iamwwc <qaq1362211689@gmail.com>
2021-09-21 18:31:56 +0800

    nat translator

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::{error::Error, io::{self, ErrorKind}, net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr}};
+use std::{error::Error, io::{self, ErrorKind}, net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4}};
 use ipnet::Ipv4Net;
 use log::{
     error
 };
-use etherparse::{IpHeader, PacketHeaders, ReadError, TransportHeader};
+use etherparse::{IpHeader, Ipv4Header, PacketBuilder, PacketHeaders, ReadError, TransportHeader};
 use tokio::io::AsyncReadExt;
 use tun::{AsyncDevice, Device, Layer};
 
@@ -55,14 +55,14 @@ impl Tun {
             Err(err) => return Err(io::Error::new(ErrorKind::Other, err))
         };
         let payload_len = ipPacket.payload.len();
-        let header = match ipPacket.ip {
-            Some(ref header) => header,
+        let mut ip_header = match ipPacket.ip {
+            Some(ref mut header) => header,
             None => {
                 error!("unknown ethernet packet {:?}", ipPacket);
                 return Err(io::Error::new(ErrorKind::Other, "unknown ethernet packet"))
             }
         };
-        let (src_ip, destination_ip): (IpAddr, IpAddr) = match header{
+        let (src_ip, destination_ip): (IpAddr, IpAddr) = match ip_header{
             IpHeader::Version4(v4) => (Ipv4Addr::from(v4.source).into(), Ipv4Addr::from(v4.destination).into()),
             IpHeader::Version6(v6) => (Ipv6Addr::from(v6.source).into(), Ipv6Addr::from(v6.destination).into())
         };
@@ -70,14 +70,31 @@ impl Tun {
         match ipPacket.transport {
             Some(TransportHeader::Tcp(ref mut tcp_header)) => {
                 // port map
-
+                let src_addr = SocketAddr::new(src_ip, tcp_header.source_port);
+                let dest_addr = SocketAddr::new(destination_ip, tcp_header.destination_port);
+                let (final_src_addr, final_dest_addr) = match self.tcp_tun.handle_packet(src_addr, dest_addr, tcp_header).await? {
+                    Some(x) => x,
+                    None => {return Ok(false)}
+                };
                 // replace src ip, port
-                
+                match (final_src_addr, &mut ip_header) {
+                    (SocketAddr::V4(v4), IpHeader::Version4(v4_header)) => v4_header.source = v4.ip().octets(),
+                    (SocketAddr::V6(v6), IpHeader::Version6(v6_header)) => v6_header.source = v6.ip().octets(),
+                    _ => unreachable!("src ip replace unreachable!")
+                };
                 // replace dest ip, port 
-                
+                match (final_dest_addr, &mut ip_header) {
+                    (SocketAddr::V4(v4), IpHeader::Version4(v4_header)) => v4_header.destination = v4.ip().octets(),
+                    (SocketAddr::V6(v6), IpHeader::Version6(v6_header)) => v6_header.destination = v6.ip().octets(),
+                    _ => unreachable!("dest ip replace unreachable!")
+                }
                 // calculate tcp checksum
-                
+                match ip_header {
+                    IpHeader::Version4(v4_ip_header) => tcp_header.checksum = tcp_header.calc_checksum_ipv4(&v4_ip_header, ipPacket.payload).expect("tcp calculate check sum error"),
+                    IpHeader::Version6(v6_ip_header) => tcp_header.checksum = tcp_header.calc_checksum_ipv6(&v6_ip_header, ipPacket.payload).expect("tcp calculate check sum error"),
+                }
                 // write ip header and tcp header into transport
+                ip_header.wr
             },
             Some(TransportHeader::Udp(ref mut udp_header)) => {
                 
