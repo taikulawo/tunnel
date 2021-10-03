7bb8efd865bbdc754f2c98923c18ba38f9c57440
iamwwc <qaq1362211689@gmail.com>
2021-10-03 16:05:54 +0800

    full socks5 implementation

--- src/proxy/socks/mod.rs
+++ src/proxy/socks/mod.rs
@@ -1,11 +1,27 @@
-use crate::proxy::{
-    TcpInbound,
-    ProxyStream
-};
+use tokio::io::AsyncWriteExt;
+
+use crate::proxy::{AnyStreamTrait, ConnectionSession, ProxyStream, TcpInbound};
 
 mod inbound;
 mod outbound;
 
 pub struct Socks {
     
+}
+
+// as client
+async fn handshake_as_client<T>(stream: &mut T)
+where
+    T: AnyStreamTrait,
+{
+    stream.write_all(&[0x05, 0x01, 0x00]).await;
+    todo!()
+}
+
+// as server
+async fn handshake_as_server<T>(stream: &mut T) -> ConnectionSession
+where
+    T: AnyStreamTrait
+{
+    todo!()
 }
