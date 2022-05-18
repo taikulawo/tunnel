017d2211c5563466274a228cf719dab0defd1556
iamwwc <qaq1362211689@gmail.com>
2022-05-18 22:53:03 +0800

    add unit test

--- tests/server.rs
+++ tests/server.rs
@@ -2,8 +2,9 @@
 // 所以 local-proxy inbound 只需要socks就行，
 // 其他协议的 inbound，通过 local-proxy#outbound => remote-proxy-server#inbound 测试
 
-use std::net::SocketAddr;
+use std::{net::{SocketAddr, Ipv4Addr, ToSocketAddrs}, str::FromStr};
 
+use futures::{FutureExt, future::BoxFuture};
 use tokio::{net::{
     TcpListener,
     UdpSocket
@@ -25,7 +26,6 @@ pub async fn tcp_echo_server(addr: SocketAddr) {
         }
     }
 }
-
 pub async fn udp_echo_server(bind_addr: SocketAddr) {
     let socket = UdpSocket::bind(bind_addr).await.unwrap();
     let mut buf = Vec::new();
@@ -46,12 +46,26 @@ pub async fn udp_echo_server(bind_addr: SocketAddr) {
     }
 }
 
+pub fn run_two_of_echo_server(bind_addr: SocketAddr) -> Vec<BoxFuture<'static, ()>>{
+    let mut tasks = Vec::new();
+    let f = tcp_echo_server(bind_addr.clone()).boxed();
+    tasks.push(f);
+    let f = udp_echo_server(bind_addr.clone()).boxed();
+    tasks.push(f);
+    tasks
+}
 pub async fn start_tunnel(config: tunnel::Config) {
     let rt = Builder::new_current_thread().enable_all().build().unwrap();
-    let tasks = start_instance(config).unwrap();
+    let mut tasks = start_instance(config).unwrap();
+    
+    // echo server is the real remote server we want to connected.
+    let mut echo_futures = run_two_of_echo_server(SocketAddr::from_str("127.0.0.1:6666").unwrap());
+    tasks.append(&mut echo_futures);
+    
+    // tasks.extend_from_slice();
     let (abort_future, abort_handler) = futures::future::abortable(futures::future::join_all(tasks));
     let test_future = async {
-
+        
         // call abort handler after test completed
         abort_handler.abort();
     };
